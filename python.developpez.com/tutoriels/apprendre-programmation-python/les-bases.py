660959ce00adfed80da20f2afb34377dd79cbd04
Florian Garcia <32206787+FloG16@users.noreply.github.com>
2021-07-30 14:32:42 +0200

    Create les-bases.py

---
+++ python.developpez.com/tutoriels/apprendre-programmation-python/les-bases.py
@@ -0,0 +1,9 @@
+#Une fois n'est pas coutume, lorsqu'il s'agit de présenter la syntaxe d'un langage de programmation, on utilise comme exemple le programme Hello World. Il s'agit simplement d'un programme 
+#qui affiche les deux mots « Hello World » à l'écran, avant de se terminer.
+
+#Voici ce que cela donne en Python :
+
+print('Hello World!')
+
+#Quoi de plus simple ? Python inclut une fonction prédéfinie print qui affiche à l'écran la séquence de caractères qu'on lui fournit. 
+#Cette unique ligne de code constitue notre premier programme Python.*/
