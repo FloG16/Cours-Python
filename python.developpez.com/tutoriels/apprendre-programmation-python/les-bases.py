cd45b6ef90e069d5559a4e76aa3c8e792b3a4448
Florian Garcia <32206787+FloG16@users.noreply.github.com>
2021-07-31 09:54:46 +0200

    Update les-bases.py

--- python.developpez.com/tutoriels/apprendre-programmation-python/les-bases.py
+++ python.developpez.com/tutoriels/apprendre-programmation-python/les-bases.py
@@ -8,3 +8,41 @@ print('Hello World!')
 #Quoi de plus simple ? Python inclut une fonction prédéfinie print qui affiche à l'écran la séquence de caractères qu'on lui fournit. 
 #Cette unique ligne de code constitue notre premier programme Python.
 
+#Voyons maintenant un exemple de programme qui utilise plusieurs variables pour calculer le discriminant du trinôme x2+2x−4
+
+a = 1
+b = 2
+c = -4
+
+delta = b ** 2 - 4 * a * c
+print('Le discriminant est :')
+print(delta)
+
+#Les trois premières instructions stockent les coefficients du trinôme, à savoir 1, 2 et −4, respectivement dans des variables nommées a, b et c. 
+#La quatrième instruction effectue le calcul du discriminant (b2−4ac) et stocke le résultat dans la variable delta. Enfin, les deux dernières instructions affichent
+#à l'écran la phrase « Le discriminant est : », suivie de la valeur de la variable delta.
+
+#L'utilisation d'une variable est donc assez immédiate. Deux types d'accès peuvent être réalisés :
+
+    #Pour affecter une valeur à une variable, c'est-à-dire l'initialiser ou modifier sa valeur, on utilise l'opérateur d'affectation (=). À gauche de l'opérateur, on retrouve le nom de la variable et à droite la valeur qu'on souhaite lui affecter.
+    #Pour accéder au contenu d'une variable, il suffit d'utiliser son nom. Cet accès peut, par exemple, se faire dans une expression mathématique, ou avec la fonction print.
+
+#Le symbole = est à distinguer du signe égal utilisé en mathématiques pour énoncer une égalité. En Python, il permet d'affecter une valeur à une variable. Voyons cela avec l'exemple suivant :
+
+a = 1
+b = a
+print(a)
+print(b)
+
+a = 15
+print(a)
+print(b)
+
+#En mathématiques, écrire b=a signifie que a et b représentent la même valeur. Dès lors, lorsqu'on écrit plus loin a = 15, on pourrait s'attendre à ce que a et b 
+#valent 15
+
+#En Python, b = a signifie qu'on affecte la valeur de a à la variable b (on fait une copie de la valeur). Après exécution de cette instruction, 
+#aucun lien n'est établi entre les deux variables. L'exécution du programme affiche lors de son exécution :
+ 
+
+
