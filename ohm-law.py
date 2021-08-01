d643934a7dbc39106333e15810c858b674bf692b
Florian Garcia <32206787+FloG16@users.noreply.github.com>
2021-08-01 20:28:46 +0200

    Update ohm-law.py

--- ohm-law.py
+++ ohm-law.py
@@ -19,3 +19,38 @@ else:
 print('U =', U, '[V]')
 print('I =', I, '[A]')
 print('R =', R, '[Ω]')
+
+--------------------------------------------------------------------
+#Autre solution
+
+while (1):
+    print("1. Calculer Voltage")
+    print("2. Calculer Resistance")
+    print("3. Calculer Intensite")
+    print("4. Sortir du programme")
+    ask = input("> ")
+    if (ask == "1"):
+        print("-- Calcul du voltage --")
+        i = float(input("Intensite: "))
+        r = float(input("Resistance: "))
+        v = i * r
+        print("Voltage = ", format(v, ".2f"))
+
+    elif(ask == "2"):
+        print("-- Calcul de la  resistance --")
+        v = float(input("Voltage: "))
+        i = float(input("Intensite: "))
+        r = v / i
+        print("Resistance = ", format(r, ".2f"))
+
+    elif(ask == "3"):
+        print("-- Calcul de l'intensite --")
+        v = float(input("Voltage: "))
+        r = float(input("Resistence: "))
+        i = v / r
+        print("Intensite= ", format(i, ".2f"))
+
+    elif(ask == "4"):
+        break
+    else:
+        print("FIN")
