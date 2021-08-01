9a4c44d7fb6e5eb0febf880a3e7f277da75d94f7
Florian Garcia <32206787+FloG16@users.noreply.github.com>
2021-08-01 21:02:30 +0200

    Create NF EN 1992-1-1.py

---
+++ Genie-Civil/NF EN 1992-1-1.py
@@ -0,0 +1,45 @@
+#Page 22
+#EN 1992-1-1:2004
+#2.3.2 Propriétés des matériaux et des produits
+#2.3.2.1 Généralités
+#(1) Les règles relatives aux propriétés des matériaux et des produits sont données dans l’EN 1990 Section 4.
+#(2) Les dispositions relatives au béton, à l’acier de béton armé et à l’acier de précontrainte sont données à la
+#Section 3 ou dans la Norme de Produit concernée.
+#2.3.2.2 Retrait et fluage
+#(1) Le retrait et le fluage sont des propriétés du béton dépendantes du temps. Il convient généralement de tenir
+#compte de leurs effets pour la vérification aux états-limites de service.
+#(2) Pour les états-limites ultimes, il convient de ne considérer les effets du retrait et du fluage que lorsque ceux-ci
+#sont significatifs — dans la vérification des états-limites ultimes de stabilité, par exemple, lorsque les effets du
+#second ordre ont leur importance. Dans les autres cas, il n’y a pas lieu de prendre ces effets en considération pour les
+#états-limites ultimes, sous réserve toutefois que la ductilité et la capacité de rotation des éléments soient suffisantes.
+#(3) Lorsque le fluage est pris en compte, il convient d’en évaluer les effets, dans le dimensionnement, sous la
+#combinaison  quasi-permanente  des  actions,  indépendamment  de  la  situation  de  projet  considérée  —  durable,
+#transitoire ou accidentelle.
+#NOTE  Dans la plupart des cas, les effets du fluage peuvent être évalués sous charges permanentes et pour la valeur
+#moyenne de la précontrainte.
+#2.3.3 Déformations du béton
+#(1)P Les conséquences des déformations dues à la température, au fluage et au retrait doivent être prises en
+#considération dans le calcul.
+#(2) Le respect des Règles d’Application de la présente Norme permet normalement de tenir compte de ces
+#effets. Il convient également :
+#— de minimiser les déformations et la fissuration dues à l’évolution du béton au jeune âge, au fluage et au retrait,
+#par une formulation adaptée ;
+#— de minimiser les gênes apportées aux déformations par des appareils d’appui ou des joints appropriés ;
+#— en cas de déformations gênées, de veiller à la prise en compte de leur influence dans le calcul.
+#(3) Dans les bâtiments, les effets de la température et du retrait peuvent être négligés dans l’analyse globale
+#sous réserve que des joints, espacés de djoint, soient incorporés afin de reprendre les déformations résultantes.
+#NOTE  La valeur djoint relève de l’Annexe Nationale. La valeur recommandée est djoint = 30 m. Par comparaison avec
+#les structures coulées en place, la valeur peut être supérieure pour les structures préfabriquées en béton, une partie du
+#fluage et du retrait se produisant alors avant la mise en œuvre des éléments. 
+#2.3.4 Données géométriques
+#2.3.4.1 Généralités
+#(1) Les règles relatives aux données géométriques sont données dans l’EN 1990 Section 4.
+#2.3.4.2 Exigences complémentaires pour les pieux coulés en place
+#(1)P Les incertitudes liées à la section droite des pieux coulés en place ainsi qu’aux procédures de bétonnage
+#doivent être prises en compte dans le calcul. 
+#(2) Dans le cas des pieux coulés en place sans tubage définitif, il convient, en l’absence d’autres dispositions,
+#d’adopter, pour les calculs de dimensionnement, les valeurs de diamètre suivantes :
+#— si dnom < 400 mm d = dnom – 20 mm 
+#— si 400 ≤ dnom ≤ 1 000 mm d = 0,95.dnom 
+#— si dnom > 1 000 mm d = dnom – 50 mm
+#dnom étant le diamètre nominal du pieu"""
